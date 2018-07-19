0581157d9ce4716fc93ed83df45a085bbb074d8d
程珊珊 <chengshanshan@clinicalservice.cn>
2018-07-19 11:18:45 +0800

    基本组件

---
+++ app/js/core/btn.vue
@@ -0,0 +1,35 @@
+/*
+ * @Author: chengshanshan 
+ * @Date: 2018-07-19 11:10:18 
+ * @Last Modified by: chengshanshan
+ * @Last Modified time: 2018-07-19 11:16:45
+ */
+
+<template lang="html">
+    <div :class="[btnClass,cname]">
+       <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        cname:{
+            type:String,
+            default:""
+        }
+    },
+    data(){
+        return {
+            btnClass:'btn',
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import "../../css/element.scss";
+.btn{
+    @include btn;
+}
+</style>
+
